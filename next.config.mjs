9723f5f3ef073fdea9adf9a951c6e8caac0a3cb0
shopia-sal <shopiasalma599@gmail.com>
2025-05-24 10:52:33 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Definisikan __dirname secara manual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -9,6 +16,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname),
+    };
+    return config;
+  },
 }
 
-export default nextConfig
+export default nextConfig;
